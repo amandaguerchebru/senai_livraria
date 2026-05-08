69ef9aa1cc43fc56bc8d74727036a39e29ca03ea
Amanda Guerche Bruzadin <amanda.bruzadin@aluno.senai.br>
2026-05-08 09:44:36 -0300

    intervalo - manhã

---
+++ app/models/Livro.php
@@ -0,0 +1,13 @@
+<!-- por ser classe, o arquivo começa com letra maiúscula -->
+ <?php
+
+class Livro
+{
+    private $id;
+    private $titulo;
+    private $ano;
+    private $generoId;
+    private $generoNome;
+    private $quantidade;
+    private $statusLivro;
+}
